98b48a2da27188c72db4dbe81776e4bfc21ca95f
dspdamith <dspdamithse@gmail.com>
2019-05-25 14:12:27 +0530

    Add project files.

---
+++ HealthConsulting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthConsulting.Startup))]
+namespace HealthConsulting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
